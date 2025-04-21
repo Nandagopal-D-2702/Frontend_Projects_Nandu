8247cf7ba154887d1889199ba52cff9149c20d1f
Nandagopal-D-2702 <Nandugopaldevraj.2702@gmail.com>
2025-04-21 20:26:47 +0530

    Dashboard Project

---
+++ Dashboard/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Nandagopal</span>
+      <span>© Nandagopal Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ Dashboard/src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+  return (
+    <div>
+      <div>Login</div>
+    </div>
+  )
+}
+
+export default Login
